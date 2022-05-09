13647e61731a7eaa7a67e1d49ca77070318865d5
Anjana <58140600+anjana-kt@users.noreply.github.com>
2022-05-09 23:12:42 +0530

    Update detect_face.py

--- detect_face.py
+++ detect_face.py
@@ -10,22 +10,31 @@ def detect_a_face ():
 
         while True :
                 _, img = cap.read()
+                b=False
+                if not _:
+                        print("Can't receive frame (stream end?). Exiting ...")
+                        break
 
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
                 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
+        
                 for (x, y, w, h) in faces:
                         #cv2.ellipse(img, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
                         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
                 if len(faces) !=0 : 
                         t1 = time.time()
-                        return True
+                        b=True
+                        return b
 
+                k = cv2.waitKey(30) & 0xff
+                if k==27:
+                        return b
                         
                 cv2.imshow('img', img)
 
                 cap.release()
+                return b
 
 print(detect_a_face())
