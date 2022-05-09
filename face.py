cd28273725c713237f794ff7b135d56aa3fd3948
Anjana <58140600+anjana-kt@users.noreply.github.com>
2022-05-09 22:52:00 +0530

    Update face.py

--- face.py
+++ face.py
@@ -2,9 +2,8 @@ import cv2
 import dlib
 import time
 
-count,t1,t2=0,0,0
+t1,t2=0,0
 l=0
-prev=count
 flag=0
 idp = 0
 data =[]
@@ -18,6 +17,9 @@ cap = cv2.VideoCapture(0)
 while True:
     _, img = cap.read()
 
+    if not _:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
@@ -26,18 +28,17 @@ while True:
         #cv2.ellipse(img, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    if len(faces) !=0 and prev==count: 
-            prev=count
-            count+=1
+    if len(faces) !=0 and flag==0: 
+            flag=1
             t1 = time.time()
 
-    elif len(faces)==0 and prev!=count:
+    elif len(faces)==0 and flag!=0:
             idp+=1
-            prev=count
+            flag=0
             t2=time.time()
             data.append([idp,t2-t1])
             print(data)
-            flag=1
+            
         
     cv2.imshow('img', img)
 
@@ -47,9 +48,8 @@ while True:
         l=len(faces)
         break
 
-if l!=0 and prev!=count:
+if l!=0 and flag!=0:
             idp+=1
-            prev=count
             t2=time.time()
             data.append([idp,t2-t1])
             print(data)       
